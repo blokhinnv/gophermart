06da761bca0f76af3442faa0ddf100331a694a44
blokhinnv <darenne@yandex.ru>
2023-02-10 13:28:51 +0300

    added default jwt sign

--- internal/app/server/config/config.go
+++ internal/app/server/config/config.go
@@ -11,8 +11,8 @@ type Config struct {
 	RunAddress           string        `env:"RUN_ADDRESS"`
 	DatabaseURI          string        `env:"DATABASE_URI"`
 	AccrualSystemAddress string        `env:"ACCRUAL_SYSTEM_ADDRESS"`
-	JWTSigningKey        string        `env:"JWT_SIGNING_KEY"`
-	JWTExpireDuration    time.Duration `env:"JWT_EXPIRE_DURATION"`
+	JWTSigningKey        string        `env:"JWT_SIGNING_KEY"        envDefault:"practicum"`
+	JWTExpireDuration    time.Duration `env:"JWT_EXPIRE_DURATION"    envDefault:"1h"`
 }
 
 func NewConfig() (*Config, error) {
